d703f3e13914deec5f7e4112d2c2f5c852fda931
BuildTools <unconfigured@null.spigotmc.org>
2020-11-28 23:54:09 +0100

    added possibility of uploading multiple clips at once

--- TwitchClipAutodownloader/DiscordCommands/DiscordUpload.cs
+++ TwitchClipAutodownloader/DiscordCommands/DiscordUpload.cs
@@ -6,6 +6,7 @@ using NYoutubeDL;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -38,99 +39,110 @@ namespace TwitchClipAutodownloader.DiscordCommands
         [Command("upload")]
         [Alias("up")]
         [Summary("Uploads missing clip to the bot")]
-        public async Task UploadClip(string url)
-        {            
+        public async Task UploadClip([Remainder] string urlArray)
+        {
+            List<string> links = new List<string>();
+            links = urlArray.Split('\n', ' ').ToList();
             //ulong.Parse(configuration.GetSettings("Discord_Server")), ulong.Parse(configuration.GetSettings("Discord_Channel")
             SocketGuild server = Context.Client.GetGuild(ulong.Parse(Program.configuration.GetSettings("Discord_Server")));
             // Get Channel to send clips to
             SocketTextChannel channel = server.GetChannel(ulong.Parse(Program.configuration.GetSettings("Discord_Channel"))) as SocketTextChannel;
             Database database = new Database(Program.configuration.GetConnectionString("Clips"));
             string clipId = "";
-            if (url.StartsWith("clips.twitch.tv"))
+            IDisposable typingDM = Context.Channel.EnterTypingState();
+            await Context.Channel.SendMessageAsync("Now starting upload of " + links.Count + " clip/s");
+            try
             {
-                clipId = url.Split("/")[1];
-            }
-            else if (url.StartsWith("http"))
-            {
-                clipId = url.Split("//")[1].Split("/")[1];
-            }
-            else
-            {
-                clipId = url;
-            }
-            if (clipId != "")
-            {
-                string TwitchApiUrl = "https://api.twitch.tv/helix/clips?id=" + clipId;                
-                string responseBody = await Twitch.twitch.GetAsync(TwitchApiUrl).Result.Content.ReadAsStringAsync();
-                TwitchClass streamTwitch = JsonSerializer.Deserialize<TwitchClass>(responseBody);
-                if (streamTwitch.data.Count != 0)
+                foreach (string url in links)
                 {
-                    ClipInfo clip = streamTwitch.data[0];
-                    if (clip.broadcaster_id == Program.configuration.GetSettings("Broadcaster_ID"))
+                    if (url.StartsWith("clips.twitch.tv"))
                     {
-                        DateTime currentTime = DateTime.UtcNow;
-                        DateTime oneDayAgo = currentTime.AddDays(-1);
-                        if (clip.created_at < oneDayAgo)
-                        {
-                            YoutubeDL twitchDownload = new YoutubeDL();
-                            string path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "/clips";
-                            await database.OpenDBConnection();
-                            if (ignoreDB || (await database.CheckIfClipAlreadyExists(clip.id)) == false)
-                            {
-                                string tempPath = path + $"/{clip.id}.mp4";
-                                try
-                                {                                    
-                                    twitchDownload.Options.FilesystemOptions.Output = tempPath;
-                                    twitchDownload.VideoUrl = clip.url;
-                                    IDisposable typing = channel.EnterTypingState();
-                                    await twitchDownload.DownloadAsync();
-                                    await channel.SendFileAsync(tempPath, "", false, CreateEmbed(clip));
-                                    typing.Dispose();
-
-                                    await database.ClipToDatabase(clip);
-                                    await Context.Channel.SendMessageAsync("Clip uploaded successfully");
-                                    Program.Logging.Log("User " + Context.User.Username + "(id: " + Context.User.Id + ")" + " uploaded a Clip");
-                                }
-                                catch (Exception ex)
-                                {
-                                    Program.Logging.Log(ex.Message);
-                                    EmbedBuilder error = new EmbedBuilder()
-                                    {
-                                        Description = ex.StackTrace
-                                    };
-                                    await Context.Channel.SendMessageAsync("There was an Exception, Please send the following Message to Donad_VR", false, error.Build());
-                                }
-                                finally
-                                {
-                                    if (File.Exists(tempPath))
-                                    {
-                                        File.Delete(tempPath);
-                                    }
-                                }                                
-                            }
-                            else
-                            {
-                                await Context.Channel.SendMessageAsync("Clip already exists");
-                            }
-                            await database.CloseDBConnection();
-                        }
-                        else
-                        {
-                            DateTime creation = clip.created_at.AddDays(1);
-                            await Context.Channel.SendMessageAsync("Clip has to be older than 24 hours, you can request the clip at " + creation.ToString("dd.MM.yyyy hh:mm:ss tt") + " UTC");
-                        }
+                        clipId = url.Split("/")[1];
+                    }
+                    else if (url.StartsWith("http"))
+                    {
+                        clipId = url.Split("//")[1].Split("/")[1];
                     }
                     else
                     {
-                        await Context.Channel.SendMessageAsync("The clip has to be from Sean_VR");
+                        clipId = url;
                     }
-                    
+                    if (clipId != "")
+                    {
+                        string TwitchApiUrl = "https://api.twitch.tv/helix/clips?id=" + clipId;
+                        string responseBody = await Twitch.twitch.GetAsync(TwitchApiUrl).Result.Content.ReadAsStringAsync();
+                        TwitchClass streamTwitch = JsonSerializer.Deserialize<TwitchClass>(responseBody);
+                        if (streamTwitch.data.Count != 0)
+                        {
+                            ClipInfo clip = streamTwitch.data[0];
+                            if (clip.broadcaster_id == Program.configuration.GetSettings("Broadcaster_ID"))
+                            {
+                                DateTime currentTime = DateTime.UtcNow;
+                                DateTime oneDayAgo = currentTime.AddDays(-1);
+                                if (clip.created_at < oneDayAgo)
+                                {
+                                    YoutubeDL twitchDownload = new YoutubeDL();
+                                    string path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "/clips";
+                                    await database.OpenDBConnection();
+                                    if (ignoreDB || (await database.CheckIfClipAlreadyExists(clip.id)) == false)
+                                    {
+                                        string tempPath = path + $"/{clip.id}.mp4";
+                                        try
+                                        {
+                                            twitchDownload.Options.FilesystemOptions.Output = tempPath;
+                                            twitchDownload.VideoUrl = clip.url;
+                                            IDisposable typing = channel.EnterTypingState();
+                                            await twitchDownload.DownloadAsync();
+                                            await channel.SendFileAsync(tempPath, "", false, CreateEmbed(clip));
+                                            typing.Dispose();
+
+                                            await database.ClipToDatabase(clip);
+                                            await Context.Channel.SendMessageAsync("Clip " + clip.id + " uploaded successfully");
+                                            Program.Logging.Log("User " + Context.User.Username + "(id: " + Context.User.Id + ")" + " uploaded a Clip (ID: " + clip.id + ")");
+                                        }
+                                        catch (Exception ex)
+                                        {
+                                            Program.Logging.Log(ex.Message + "\n" + ex.StackTrace);
+                                        }
+                                        finally
+                                        {
+                                            if (File.Exists(tempPath))
+                                            {
+                                                File.Delete(tempPath);
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        await Context.Channel.SendMessageAsync("The Clip \"" + clip.id + "\" already exists");
+                                    }
+                                    await database.CloseDBConnection();
+                                }
+                                else
+                                {
+                                    DateTime creation = clip.created_at.AddDays(1);
+                                    await Context.Channel.SendMessageAsync("Clip (id: " + clip.id + ") has to be older than 24 hours, you can request the clip at " + creation.ToString("dd.MM.yyyy hh:mm:ss tt") + " UTC");
+                                }
+                            }
+                            else
+                            {
+                                await Context.Channel.SendMessageAsync("The clip (id: " + clip.id + ") has to be from Sean_VR");
+                            }
+
+                        }
+                        else
+                        {
+                            await Context.Channel.SendMessageAsync("Clip (id: " + clipId + ") not found");
+                        }
+                    }
                 }
-                else
-                {
-                    await Context.Channel.SendMessageAsync("Clip not found");
-                }                                
             }
+            catch (Exception ex)
+            {
+                Program.Logging.Log(ex.Message + "\n" + ex.StackTrace);
+            }            
+            typingDM.Dispose();
+            await Context.Channel.SendMessageAsync("Finished");
         }
         /// <summary>
         /// Embed creator to build a better Message
--- TwitchClipAutodownloader/Twitch.cs
+++ TwitchClipAutodownloader/Twitch.cs
@@ -90,9 +90,9 @@ namespace TwitchClipAutodownloader
                             {
                                 DoubleCheck = true;
                             }
-                            Task work1 = ConfigClipSearch(database, clientPassthrough, false, currentTime.AddMinutes(-30), DoubleCheck);
-                            Task work2 = ConfigClipSearch(database, clientPassthrough, false, currentTime.AddMinutes(-40), DoubleCheck);
-                            Task work3 = ConfigClipSearch(database, clientPassthrough, false, currentTime.AddMinutes(-50), DoubleCheck);
+                            Task work1 = ConfigClipSearch(database, clientPassthrough, false, currentTime.AddMinutes(-20), DoubleCheck);
+                            Task work2 = ConfigClipSearch(database, clientPassthrough, false, currentTime.AddMinutes(-30), DoubleCheck);
+                            Task work3 = ConfigClipSearch(database, clientPassthrough, false, currentTime.AddMinutes(-40), DoubleCheck);
                             // await ConfigClipSearch(database, configuration, clientPassthrough, false, currentTime);
                             await Task.WhenAll(work1, work2, work3);
                             if (clips.Count > 0)
